16b543993868e571f0ad0233fb46e5b2a7eadd87
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-12-14 15:49:51 +0530

    Logic and css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function App() {
-  return <div>App</div>;
+  const [total, setTotal] = useState(0);
+  const [input, setInput] = useState(0);
+  const handleAddition = () => {
+    setTotal(total + parseFloat(input));
+    setInput(0);
+  };
+  const handleSubstraction = () => {
+    setTotal(total - parseFloat(input));
+    setInput(0);
+  };
+  const handleMultiplication = () => {
+    setTotal(total * parseFloat(input));
+    setInput(0);
+  };
+  const handleDivision = () => {
+    setTotal(total / parseFloat(input));
+    setInput(0);
+  };
+  return (
+    <div>
+      <h1>Calculator</h1>
+      <h2>{total}</h2>
+      <input
+        type="number"
+        placeholder="enter"
+        name="value"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        autoFocus
+      />
+      <ul className="calOptions">
+        <li>
+          <button onClick={handleAddition}>Add</button>
+        </li>
+        <li>
+          <button onClick={handleSubstraction}>Subtract</button>
+        </li>
+        <li>
+          <button onClick={handleMultiplication}>Multiply</button>
+        </li>
+        <li>
+          <button onClick={handleDivision}>Divide</button>
+        </li>
+        <li>
+          <button className="rstInput">Reset Input</button>
+        </li>
+        <li>
+          <button className="rstResult">Reset result</button>
+        </li>
+      </ul>
+    </div>
+  );
 }
